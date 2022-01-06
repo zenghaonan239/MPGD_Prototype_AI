3ca762de48f17282b64c74e8d4100de3b4bc95aa
haonanzeng <56869544+haonanzeng@users.noreply.github.com>
2022-01-06 03:41:06 +0000

    UI
    jump UI Setting UI

--- Assets/Scripts/UIControl.cs
+++ Assets/Scripts/UIControl.cs
@@ -5,12 +5,44 @@ using UnityEngine.SceneManagement;
 
 public class UIControl : MonoBehaviour
 {
+    public GameObject ReturnUI;
+    public GameObject SettingUI;
+
     public void onTutorial()
     {
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(0);//load tutorial scene 
+    }
+
+    public void onStart()
+    {
+        SceneManager.LoadScene(2);//load Game scene 
     }
 
 
+    public void onReturn()//when press return button 
+    {
+        ReturnUI.SetActive(true);
+    }
+
+    public void onResume()//when press resume button
+    {
+        ReturnUI.SetActive(false);
+    }
+
+    public void onReturnToMain()//go back to main menu
+    {
+        SceneManager.LoadScene(1);//load main menu scene 
+    }
+
+    public void onSetting()//when press return button 
+    {
+        SettingUI.SetActive(true);
+    }
+
+    public void onReturnFromSetting()//when press return button 
+    {
+        SettingUI.SetActive(false);
+    }
 
 
 }
